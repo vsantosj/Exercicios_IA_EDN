b395b52e57e94cdd234e357782ba40c597d56875
viviane santos <vjsantos.viviane@gmail.com>
2025-05-06 01:38:13 -0300

    adding folder aula01  with file age_conditions.py and price_with_discounted.py

---
+++ aula01/atividades/age_conditions.py
@@ -0,0 +1,6 @@
+age = 20
+
+if (age >= 18):
+    print('Maior de idade')
+else:
+    print('menor de idade')
---
+++ aula01/atividades/price_with_discounted.py
@@ -0,0 +1,5 @@
+price = 1000
+discount = 15
+
+final_value = price * 0.85
+print(f'Preço final do celular de Maria com {discount}% de desconto é R${final_value}')
