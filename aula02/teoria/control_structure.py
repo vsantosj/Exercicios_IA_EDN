ee4fda70c2583e8dc73ffa9f8c7e9a8ded29ad06
viviane santos <vjsantos.viviane@gmail.com>
2025-05-12 12:06:16 -0300

    adding file control_structure.py

---
+++ aula02/teoria/control_structure.py
@@ -0,0 +1,25 @@
+#se está chovendo -> levo guarda-chuva
+#Senão, se está ensolarado -> passo o protetor solar
+#senão -> Ando de boa na lagoa
+
+#em python
+its_raining = True
+its_sunny = True
+color_umbrella = "black"
+
+def carry_umbrella(color):
+    return
+
+def apply_sunscreen():
+    return
+
+def i_m_calm():
+    return
+
+
+if its_raining:
+    carry_umbrella(color_umbrella)
+elif its_sunny:
+    apply_sunscreen()
+else:
+    i_m_calm()
