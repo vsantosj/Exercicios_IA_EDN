e5619666870aa01f95c275288b362fdfd3a20c70
viviane santos <vjsantos.viviane@gmail.com>
2025-05-12 11:31:40 -0300

    adding file even_number.py

---
+++ aula02/even_number.py
@@ -0,0 +1,6 @@
+number = int(input('Digite um número para saber se é par ou impar'))
+
+if (number % 2 == 0):
+    print('Você digitou um número par')
+else:
+    print('Você digitou um número impar')
