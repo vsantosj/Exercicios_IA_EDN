5519566c1f221a05e5122e1135e538d280002789
viviane santos <vjsantos.viviane@gmail.com>
2025-05-12 11:28:58 -0300

    addig file age_classification.py

---
+++ aula02/age_classification.py
@@ -0,0 +1,12 @@
+age = int(input('Digite sua idade'))
+
+if (age >= 65):
+    print('Você é idoso')
+elif (age >= 18):
+    print('Você é adulto')
+elif (age >= 12):
+    print('Você é adolescente')
+elif (age >= 3):
+    print('Você é uma criança')
+else:
+    print('Você é um bebê')
