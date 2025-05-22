5cd697b7f80340a87613833fc42a415a6f8cd41c
viviane santos <vjsantos.viviane@gmail.com>
2025-05-22 14:26:51 -0300

    add folder aula05

--- aula05/teoria/request_mode.py
+++ aula05/teoria/request_mode.py
@@ -2,9 +2,9 @@ import requests
 
 response = requests.get('https://www.python.org')
 
-if response.status_code == 200
+if response.status_code == 200:
     print('Deu certo')
 
     print(response.text[:100])
 else:
-    print((f'Não deu certo {response.status_code}'))
+    print(f'Não deu certo {response.status_code}')
